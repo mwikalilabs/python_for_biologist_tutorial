75745d0db8e5e746eb0ff1638d0a9e433070f5a1
Sharon Mwikali <mwikalisharon749@gmail.com>
2026-08-17 13:51:36 +0300

    Create lesson2.py

---
+++ lesson2.py
@@ -0,0 +1,31 @@
+#print("Hello Bioinformatics")
+# print("DNA")
+# print("RNA")
+# print("Protein")
+
+
+
+# #variable
+# dna_seq = "ATGGTAC"
+# print(dna_seq)
+
+
+# gene_name = "BRCA1"
+# print("The gene we are using is :", gene_name)
+
+
+# x = 10
+# y = 5
+# print (x + y)
+
+dna = "ATGGTAC"
+# print(len(dna))
+print("ATC" in dna)# to check if a substring is present in the string or not
+
+"""
+in this course i learned:
+    - Introduction to Python programming
+    - Basic data types and variables
+    - String manipulation
+
+"""
